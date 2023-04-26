35dd56af5e730fec5141415397bc39629bfde6d7
lejlahadzovic <hadzoviclejla6@gmail.com>
2023-04-26 21:10:19 +0200

    Ispravka 2-way autentifikacija

--- AngularApp/src/app/two-f-otkljucaj/two-f-otkljucaj.component.ts
+++ AngularApp/src/app/two-f-otkljucaj/two-f-otkljucaj.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
-import {MatDialog} from "@angular/material/dialog";
 import {MojConfig} from "../MojConfig";
 
 @Component({
@@ -12,15 +11,15 @@ import {MojConfig} from "../MojConfig";
 export class TwoFOtkljucajComponent implements OnInit {
 
 
-  code: string="";
-  constructor(private httpKlijent: HttpClient, private router: Router,private dialog: MatDialog ) { }
+public code: string='';
+  constructor(private httpKlijent: HttpClient, private router: Router ) { }
 
   ngOnInit(): void {
 
   }
 
   otkljucaj() {
-    this.httpKlijent.get(MojConfig.adresa_servera+ "/Autentifikacija/Otkljucaj/"+this.code,MojConfig.http_opcije()).subscribe(x=>{
+    this.httpKlijent.get(MojConfig.adresa_servera+ "/Autentifikacija/Otkljucaj/"+this.code, MojConfig.http_opcije()).subscribe(x=>{
       this.router.navigateByUrl("/");
     });
   }
